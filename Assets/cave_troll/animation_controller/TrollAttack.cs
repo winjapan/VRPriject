69e8fb9c60df87bbb70f84fd90b16380cfeae7a0
カウピーTWICE大好き <gensikaku@gmail.com>
2019-11-12 14:54:53 +0900

    add

---
+++ Assets/cave_troll/animation_controller/TrollAttack.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class TrollAttack : StateMachineBehaviour
+{
+    Animator animator;
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex, AnimatorControllerPlayable controller)
+    {
+        animator.ResetTrigger("attack1");
+    }
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (Input.GetMouseButton(0))
+        {
+            animator.SetBool("attack1", true);
+
+        }
+    }
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (stateInfo.IsName("attack2"))
+        {
+            animator.ResetTrigger("attack2");
+        }
+
+    }
+}
