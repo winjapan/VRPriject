ae71f58b70ad928eefb083c7ae99a78a9dfc9959
カウピーTWICE大好き <gensikaku@gmail.com>
2019-11-12 14:58:33 +0900

    add

---
+++ Assets/FightingMotionsVolume1/Animator/Kick.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class Kick : StateMachineBehaviour
+{
+    Animator animator;
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex, AnimatorControllerPlayable controller)
+    {
+        animator.ResetTrigger("Attack3");
+    }
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (Input.GetMouseButton(0))
+        {
+            animator.SetBool("Attack3", true);
+
+        }
+    }
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (stateInfo.IsName("FinalAttack"))
+        {
+            animator.ResetTrigger("FinalAttack");
+        }
+
+    }
+}
