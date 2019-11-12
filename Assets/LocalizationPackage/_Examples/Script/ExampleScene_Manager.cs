4ed45d40e73b7312c94987895ef705609bc0953a
カウピーTWICE大好き <gensikaku@gmail.com>
2019-11-12 15:02:56 +0900

    add

---
+++ Assets/LocalizationPackage/_Examples/Script/ExampleScene_Manager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleScene_Manager : MonoBehaviour {
+
+    public string[] AnimationNames;
+    public int CurrentIndex;
+
+	public void NextAnimation()
+    {
+        if (CurrentIndex >= AnimationNames.Length)
+            CurrentIndex = 0;
+        GetComponent<Animation>().Play(AnimationNames[CurrentIndex]);
+        CurrentIndex++;
+    }
+
+    public void OpenWeb(string web)
+    {
+        Application.OpenURL(web);
+    }
+}
